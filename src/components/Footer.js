448ce9ff66bf79f511d4c8f8b8ab9c2276af9674
Harshit Singhai <harshitsinghai77@gmail.com>
2021-06-14 23:48:19 +0530

    change footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,17 +3,6 @@ import { memo } from "react";
 const Footer = () => (
   <div className="flex-item welcome-text">
     <p>
-      Created by&nbsp;
-      <a
-        target="_blank"
-        rel="noreferrer"
-        href="https://www.linkedin.com/in/harshitsinghai/"
-      >
-        Harshit Singhai
-      </a>
-      &nbsp;©2021
-    </p>
-    <p>
       Check out my&nbsp;
       <a
         target="_blank"
@@ -29,6 +18,15 @@ const Footer = () => (
         href="https://github.com/harshitsinghai77"
       >
         GitHub
+      </a>
+    </p>
+    <p>
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://www.linkedin.com/in/harshitsinghai/"
+      >
+        Harshit Singhai &nbsp;©2021&nbsp;
       </a>
     </p>
   </div>
