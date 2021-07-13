b898971ce53e556a589f792b4ddd547852431f19
harshitsinghai77 <harshitsinghai77@gmail.com>
2021-07-13 17:44:10 +0530

    Added loader in signup page

--- src/views/google_signin.js
+++ src/views/google_signin.js
@@ -1,20 +1,25 @@
+import { useState } from "react";
 import GoogleLogin from "react-google-login";
 import { Link } from "react-router-dom";
 import { useHistory } from "react-router-dom";
 
+import { CustomSpinner } from "../components/Elements";
 import { setToken } from "../tokenStorage";
 import apiClient from "../apiClient";
 
 const CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
 const GoogleSignIn = () => {
+  const [loader, setLoader] = useState(false);
   const history = useHistory();
   const responseGoogle = async (response) => {
+    setLoader(true);
     const id_token = response.getAuthResponse().id_token;
     const res = await apiClient.user_login({ google_token: id_token });
     const { data } = res;
     if (data) {
       setToken(data["access_token"]);
+      setLoader(false);
       history.push("/");
     }
   };
@@ -35,7 +40,10 @@ const GoogleSignIn = () => {
         <div className="max-w-6xl mx-auto px-4 sm:px-6">
           <div className="pt-32 pb-12 md:pt-40 md:pb-20">
             <h1 className="h1 text-black text-5xl font-extrabold">
-              Connect with Google to get started.
+              {loader
+                ? "Please wait while we create your account"
+                : "Connect with Google to get started."}
+              {loader && <CustomSpinner />}
             </h1>
             <div className="mt-8 mb-2">{googleButton}</div>
             <Link to="/">
