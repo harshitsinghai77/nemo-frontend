f656fa7808959b91685ce608fe6eca0c78cea682
harshitsinghai77 <harshitsinghai77@gmail.com>
2022-06-24 09:02:11 +0530

    add hero pattern in settings page

--- src/views/settings/settings.js
+++ src/views/settings/settings.js
@@ -2,15 +2,17 @@ import Header from "../../components/Header";
 import { Title } from "../../components/Elements";
 import Menu from "./menu";
 
+import "../../css/settings.css";
+
 const Settings = () => {
   return (
-    <>
+    <div className="settings-hero-pattern">
       <Header textcolor />
       <div className="max-w-xl self-start m-auto">
         <Title title="Settings" color alignSelf="start" className="mb-5" />
         <Menu />
       </div>
-    </>
+    </div>
   );
 };
 
