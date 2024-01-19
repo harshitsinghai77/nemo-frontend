664ceee29034e8018175bb53b59c9b5fbb994c73
harshitsinghai77 <harshitsinghai77@gmail.com>
2024-01-20 01:42:06 +0530

    remove react helmet, add header metadata

--- src/components/countdown.js
+++ src/components/countdown.js
@@ -2,7 +2,6 @@ import { useEffect, memo } from "react";
 import { useTimer } from "react-timer-hook";
 import { Grid, Box } from "grommet";
 
-import TitleComponent from "./TitleComponent";
 import { playIcon, pauseIcon } from "../components/svg";
 import { InputTaskWhatAreYouWorkingOn } from "../components/TaskInput";
 
@@ -46,40 +45,37 @@ const Countdown = ({
   const ShowTime = `${timerHours} : ${timerMinutes} : ${timerSeconds}`;
 
   return (
-    <>
-      <TitleComponent title={`${ShowTime} remaining`} />
-      <Grid
-        rows={["xxsmall"]}
-        columns={[120, 120, 120]}
-        gap="xsmall"
-        className="pr-10"
-        areas={[
-          { name: "block1", start: [0, 0], end: [0, 0] },
-          { name: "block2", start: [1, 0], end: [1, 0] },
-          { name: "block3", start: [2, 0], end: [2, 0] },
-          { name: "block4", start: [3, 0], end: [3, 0] },
-        ]}
-      >
-        <Box gridArea="block1" className="timer-container-total-cycle">
-          {current_session}/{timer_sessions}
+    <Grid
+      rows={["xxsmall"]}
+      columns={[120, 120, 120]}
+      gap="xsmall"
+      className="pr-10"
+      areas={[
+        { name: "block1", start: [0, 0], end: [0, 0] },
+        { name: "block2", start: [1, 0], end: [1, 0] },
+        { name: "block3", start: [2, 0], end: [2, 0] },
+        { name: "block4", start: [3, 0], end: [3, 0] },
+      ]}
+    >
+      <Box gridArea="block1" className="timer-container-total-cycle">
+        {current_session}/{timer_sessions}
+      </Box>
+      <Box gridArea="block2" className="timer-container-countdown bubble">
+        {ShowTime}
+      </Box>
+      {currentTask && isRunning ? (
+        <Box gridArea="block3" className="timer-container-task bubble">
+          {truncateString(currentTask, 20)}
         </Box>
-        <Box gridArea="block2" className="timer-container-countdown bubble">
-          {ShowTime}
-        </Box>
-        {currentTask && isRunning ? (
-          <Box gridArea="block3" className="timer-container-task bubble">
-            {truncateString(currentTask, 20)}
-          </Box>
-        ) : (
-          !isRunning && <InputTaskWhatAreYouWorkingOn />
-        )}
-        <Box gridArea="block4">
-          <div className="timer-container-right-reset" onClick={onChangeActive}>
-            {isRunning ? pauseIcon : playIcon}
-          </div>
-        </Box>
-      </Grid>
-    </>
+      ) : (
+        !isRunning && <InputTaskWhatAreYouWorkingOn />
+      )}
+      <Box gridArea="block4">
+        <div className="timer-container-right-reset" onClick={onChangeActive}>
+          {isRunning ? pauseIcon : playIcon}
+        </div>
+      </Box>
+    </Grid>
   );
 };
 
